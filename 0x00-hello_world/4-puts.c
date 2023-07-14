18a317a9c6b62517db70aaf80b4e1dc70cce6639
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-14 05:02:38 -0700

    call

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  *main - the fuction for puts
  *return (0) (success)
  *\n
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - printf function
  * return 0 : (success)
  */
