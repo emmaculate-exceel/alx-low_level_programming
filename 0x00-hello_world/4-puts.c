e6ed342b67d8c615a3aff9b73d5dd8eb87bc481d
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-14 03:08:15 -0700

    vi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/*
+ * this is a function of puts 
+ * and another of 
+ * \n
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingua");
 return (0);
 }
