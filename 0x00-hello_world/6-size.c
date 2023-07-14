478addbb7d5b6a3e01d43caf334eefd1bd7e9b47
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-14 04:56:06 -0700

    clue

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main: size of bytes on a system
+ * return 0 : (success)
+ */
+int main(void)
+{
+	printf("this will print the size of \n"sizeof(unsigned long));
+	printf("this will pritt the  size of \n"sizeof(long));
+	prinf("this will print the size of \n"sizeof(short));
+	printf"this will print the size of \n"sizeof(int));
+	printf("this will print the size of \n"sizeof(char));
+	printf("this will print the size of \n"sizeof(unsigned short));
+	printf("this will print the size of \n"sizeof(unsigned int));
+	printf("this will print the size of \n"sizeof(unsigned char));
+	return (0);
+}
