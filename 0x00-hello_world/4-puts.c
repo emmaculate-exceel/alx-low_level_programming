d2d6a3544c1a13bea5fbc3c13e85be7c84316877
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-14 08:33:15 -0700

    let

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main - the fuction for puts
- *return (0) (success)
+ *main- this is the function body
+ *Returns : returns 0
  */
 int main(void)
 {
-	/* Main function body */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
