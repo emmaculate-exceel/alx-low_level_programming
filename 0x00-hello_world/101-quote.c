0bdd4933b0a9005fb561d30c6b2de5721edda332
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-16 16:34:27 -0700

    call

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - unix style
  * Return:1
  */
 int main(void)
 {
-	printl("and the piece of art is useful\" -Dora Korpar, 2015-10-19,\n");
+	fprintf(stderr, "and the piece of art is useful\" -Dora Korpar,
+	2015-10-19,\n");
 
 	return (1);
 }
