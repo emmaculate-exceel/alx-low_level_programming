5809e1bf58296e556a26879c10215dd4818e8a8a
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-16 13:43:57 -0700

    call

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %c byte(s) \n", sizeof(b));
-	printf("Size of a int: %d  byte(s) \n", sizeof(a));
+	printf("Size of a int: %d byte(s) \n", sizeof(a));
 	printf("Size of a long int: %ld byte(s) \n", sizeof(c));
 	printf("Size of a long long int: %lld byte(s) \n", sizeof(d));
 	printf("Size of a float: %f byte(s) \n", sizeof(e));
