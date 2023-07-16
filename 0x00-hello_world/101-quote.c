565737a958a8b6711039d4e3f284fb3f00ee9c5a
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-16 13:01:42 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - unix style
+ * Return:1
+ */
+int main(void)
+{
+	fputs("and the piece of art is useful\" -Dora Korpar, 2015-10-19,\n");
+	return (0);
+}
