f5f4f392d8a89fcaad1005ebf4c6407d040312ac
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-17 04:51:28 -0700

    call

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,16 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero", n);
 	}
 	else
 	{
-		printf("%-d is a negative\n", n);
+		printf("%-d is a negative", n);
 	}
-
+	printf("\n");
 	return (0);
 }
