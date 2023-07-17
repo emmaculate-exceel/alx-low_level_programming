8bb1d262a36308325bc2f4774750171089605026
emmaculate-exceel <emmaculate2excee@live.com>
2023-07-17 15:17:29 -0700

    calender

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ int main(void)
 
 	if ((n%10) > 5)
 		printf("Last digit of %d is %d and is greater than 5", n , n%10);
-	else if ((n%10) == 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n , n%10);
-	else 
+	else if ((n%10) > && (n%10) != 0)
 		printf("Last digit of %d is %d and is", n , n%10);
+	else 
+		printf("Last digit of %d is %d and is less than 6 and not 0", n , n%10);
 	printf("\n");
 
 	return (0);
