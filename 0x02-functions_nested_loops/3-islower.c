40b1b48bfb24c604f40965ff95765a830c03955a
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-19 14:15:01 -0700

    caller

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - _islower character for lowercase
+ * Return:1 for lowercase character or 0 for anything else
+ */
+
+int _islower(int c)
+{
+
+	while (c == 99 && c != 67)
+
+		return (1);
+	return (0);
+}
+
