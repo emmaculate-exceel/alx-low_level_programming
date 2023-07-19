b68da210432c7ee8290a730c27b90b8c7d995e88
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-19 14:20:55 -0700

    tears

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _islower - _islower character for lowercase
- * Return:1 for lowercase character or 0 for anything else
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 {
 
-	while (c == 99 && c != 67)
+	while (c = 99 && c > 67)
 
 		return (1);
 	return (0);
