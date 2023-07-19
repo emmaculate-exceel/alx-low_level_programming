2a7402027f7e8d06b5b0f772fce5b0c4da5c0b87
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-19 02:01:19 -0700

    can

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a to z
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		char j;
+
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+
+		_putchar(i);
+	};
+}
+
