50e8731de94d1edde1f5718c59c341c7d9836ae8
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-21 01:26:37 -0700

    engine

---
+++ 0x03-debugging/3-main_a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - takes a date and print how many days are left
+ * leap years account 
+ * Return:0
+ */
+
+int main(void)
+{
+	int day;
+	int month;
+	int year;
+
+	day = 4;
+	month = 01;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(day , month);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
