6cb05ba2f3846b28dfd1362916e21de0b37bf0ee
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-21 20:00:59 -0700

    caller

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - digits
+ * @c: clever
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+
+		return (1);
+	else
+		return (0);
+
+	return (0);
+}
