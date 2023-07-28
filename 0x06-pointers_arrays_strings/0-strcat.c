52b99f5726ca53acb859b65e4c1f4ff8c3d5c27d
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-27 18:42:10 -0700

    all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,13 @@
-#include "main.h"
 #include <string.h>
 
 /**
  * _strcat - custom function
  *@dest: concat string value
  *@src: concat string value
- *Return: 0
+ *Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-return(strcat(dest, src));
+return (strcat(dest, src));
 }
