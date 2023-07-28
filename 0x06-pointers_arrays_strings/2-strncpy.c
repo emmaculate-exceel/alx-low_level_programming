2e7f58be00a0280f328918a5d8ed52680e884ce9
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-28 16:07:38 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - check code
+ * @dest: destination
+ * @src: source
+ * @n: number of int
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+
+	return (0);
+}
