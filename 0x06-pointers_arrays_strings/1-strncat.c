8b894055257cfad0e29cffdc164836eab1525582
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-27 19:19:48 -0700

    more

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strncat - concate strings
+ * @dest: destination
+ * @src: source
+ * @n: number of value
+ * Return:0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
+			
