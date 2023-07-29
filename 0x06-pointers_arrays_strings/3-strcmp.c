9d61af4e43c74d5e97f0504384ab3f0e2dd63e10
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-28 17:20:50 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare
+ * @s1: one
+ * @s2: two
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}
