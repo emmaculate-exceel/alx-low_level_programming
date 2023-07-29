4f5ee95c0f8d38871f65f99d930d44e90ba265de
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-28 18:59:12 -0700

    caller

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse format
+ * @a: pointer 
+ * @n: value
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	n = 0;
+	while (n < a)
+	{
+		n++;
+
+	}
+
+}
