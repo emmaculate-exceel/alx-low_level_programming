b8ebf612fde83935a53d1cf5e5807e89a39d03af
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-30 13:03:51 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - task 0 debugging positive or negative
 * Return: 0
 */
 
@@ -10,7 +10,8 @@ int main(void)
 	int i;
 
 	i = 0;
-	positive_or_negative(i);
+
+	positive_and_negtive(i);
 
 	return (0);
 }
