629b55c65e8c9584138798086e16d361480239dd
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-29 21:36:03 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 
-	for (i = 0; a[i] < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(a[i]);
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
 
 	return (0);
