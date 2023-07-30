53c12f0bb91680286409ffb20d85b5dc7623e569
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-30 03:23:34 -0700

    sequence" "

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,16 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + ', ');
+		putchar(i + '0');
+
+		if (i == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
 	return (0);
 }
