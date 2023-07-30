3cc0f12e235c1d9e3b973b1165b5d18168f0f941
emmaculate-exceel <emmaculate2exceel@live.com>
2023-07-30 07:43:27 -0700

    size

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
 
 		for (j = 0; j <= 9; j++)
+			putchar(i + '0');
+			putchar(j + '9');
 		{
 			if (i == 8 && j == 9)
+			{
 				break;
-			
-			putchar(j);
+			}
+			putchar(',');
+			putchar(' ');
 
 		}
 	}
