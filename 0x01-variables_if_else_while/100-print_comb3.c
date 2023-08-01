0e8eba122958139d30227fe97144a471dde843c5
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-01 15:36:41 -0700

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,14 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 
-		for (j = 0; j <= 9; j++)
-			putchar(i + '0');
-			putchar(j + '9');
+		for (j = i + 1; j <= 9; j++)
 		{
+			putchar(i + '0');
+			putchar(j + '0');
+
 			if (i == 8 && j == 9)
 			{
 				break;
@@ -26,4 +27,6 @@ int main(void)
 
 		}
 	}
+
+	return (0);
 }
