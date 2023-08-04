4ee06da2213d334c94efff4c08aa59e62313c831
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-04 16:54:36 -0700

    static library

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code for this
+ * Return: 0
+ */
+
+int main(void)
+{
+_puts("\"At the end of the day, my goal is to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
