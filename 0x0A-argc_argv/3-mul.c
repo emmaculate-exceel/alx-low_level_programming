26364901cabe627b771bc7e2df6ec5da494f9697
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-06 22:38:02 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 	int mul1;
 	int mul2;
 	int result;
+	
+	mul1 = atoi(argv[1]);
+	mul2 = atoi(argv[2]);
 
-	if(argc == 3)
+	if (argc == 3)
 	{
-		mul1 = atoi(argv[1]);
-		mul2 = atoi(argv[2]);
-
 		result = mul1 * mul2;
 		printf("%d\n", result);
 	}
