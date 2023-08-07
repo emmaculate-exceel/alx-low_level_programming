3c146ca98e4f9f1e03b68d27ab2fb7655a521749
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-06 20:29:10 -0700

    me

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
