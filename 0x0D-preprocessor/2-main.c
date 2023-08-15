7a253ad2b2e1f4eaac85cd38213c5b86d1087f08
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-14 19:20:53 -0700

    me

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,8 @@
-#include <unistd.h>
 #include <stdio.h>
 /**
- * main - print the file name as input
+ * main - print the file name as inpiut
  * Return: 0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main(void)
 {	
 	printf("%s\n", __FILE__);
