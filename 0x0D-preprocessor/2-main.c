56e820e0aff46fd6a633def014a88d4d5c11b44e
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-14 19:06:52 -0700

    name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,15 @@
+#include <unistd.h>
 #include <stdio.h>
 /**
  * main - print the file name
  * Return: 0
  */
-
-int main(void)
+int _putchar(char c)
 {
-
+	return (write(1, &c, 1));
+}
+int main(void)
+{	
+	printf("%s\n", __FILE__);
 	return (0);
 }
