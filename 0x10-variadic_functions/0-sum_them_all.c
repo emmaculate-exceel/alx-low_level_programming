b0fa1970cf0166d088d42d8019eafb51451e5949
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-17 18:25:52 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - add all parameter
+ * @n: calculate the function
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	
+	va_list arg;
+	va_start(arg, n);
+	if (n == 0) 
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+
+	va_end(arg);
+	return (0);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Variadic functions and how to use them .
