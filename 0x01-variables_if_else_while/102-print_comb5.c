266021cda5388a854689211dcf765518616e23d7
emmaculate-exceel <emmaculate2exceel@live.com>
2023-08-21 07:48:59 -0700

    combo-5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,27 @@
 
 int main(void)
 {
-	int a ,b ,c,d ;
+	int a, b, c, d;
+
 	for (a = 0; a <= 9 ; a++)
 	{
-		for(b = 0; b <= 8; b++)
+		for (b = 0; b <= 8; b++)
 		{
-			for(c = 0; c <= 9; c++)
+			for (c = 0; c <= 9; c++)
 			{
-				for(d = 1; d <= 9; d++)
+				for (d = 1; d <= 9; d++)
 				{
-				putchar(a+'0');
-				putchar(b+'0');
-				putchar(' ');
-		  		putchar(c+'0');
-				putchar(d+'0');
-				putchar(',');
-				putchar(' ');
+				putchar (a + '0');
+				putchar (b + '0');
+				putchar (' ');
+				putchar (c + '0');
+				putchar (d + '0');
+				putchar (',');
+				putchar (' ');
 				}
-			}	
+			}
 		}
 	}
-        putchar('\n');
-	return 0;
+	putchar ('\n');
+	return (0);
 }
