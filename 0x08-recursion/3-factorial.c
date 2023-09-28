5cc398c8e42e0c3ca94ceec189183ebc51c31f3a
emmaculate-exceel <emmaculate2exceel@live.com>
2023-09-28 13:59:19 -0700

    Dream a little bigger

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,6 +2,7 @@
 /**
  * factorial - Factorial of a number
  * @n: Number of factorial
+ * Return: num
  */
 
 int factorial(int n)
@@ -9,8 +10,8 @@ int factorial(int n)
 	int num;
 
 	if (n < 0)
-		return(-1);
-	else if(n < 1 || n < 0)
+		return (-1);
+	else if (n < 1 || n < 0)
 		return(1);
 	num = n * factorial(n - 1);
 	return (num);
