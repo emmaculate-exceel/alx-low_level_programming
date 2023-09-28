4a50b0f7114dd107f70481d29967edbb3c67542c
emmaculate-exceel <emmaculate2exceel@live.com>
2023-09-28 12:22:59 -0700

    Dreams feel real

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check length of strings
+ *
+ * @c: strings
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	n = 1 + _strlen_recursion(s + 1);
+	return (n);
+}
