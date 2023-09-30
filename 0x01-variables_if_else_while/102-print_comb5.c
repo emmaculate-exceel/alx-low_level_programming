7f2e08725268a7af858be51a5532fe54d3c26c16
emmaculate-exceel <emmaculate2exceel@live.com>
2023-09-30 16:48:19 -0700

    Sofware is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,25 +7,19 @@
 
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 9 ; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0; b <= 8; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			for (c = 0; c <= 9; c++)
-			{
-				for (d = 1; d <= 9; d++)
-				{
-				putchar (a + '0');
-				putchar (b + '0');
-				putchar (' ');
-				putchar (c + '0');
-				putchar (d + '0');
-				putchar (',');
-				putchar (' ');
-				}
-			}
+		putchar ('0' + (a / 10));
+		putchar ('0' + (a % 10));
+		putchar (' ');
+		putchar ('0' + (b / 10));
+		putchar ('0' + (b % 10));
+		putchar (',');
+		putchar (' ');
 		}
 	}
 	putchar ('\n');
