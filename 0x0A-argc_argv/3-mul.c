69d55a036a22b584caed9ae9f9dc5590a44a8b3c
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-02 13:51:24 -0700

    Irony not sarcasm

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 /**
  * main - function args for multiplication
- * @argc: argument count 
+ * @argc: argument count
  * @argv: argument values
  * Return: Always 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int mul1, mul2, result;
-
-	mul1 = atoi(argv[1]);
-	mul2 = atoi(argv[2]);
-	result = mul1 * mul2;
-	printf("%d\n", result);
+	if(argc > 2)
+	{
+		mul1 = atoi(argv[1]);
+		mul2 = atoi(argv[2]);
+		result = mul1 * mul2;
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
