147a8ab4c61280b8172611c61ca6ed1c5479cf6e
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-02 10:58:34 -0700

    Silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check for argument count
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
