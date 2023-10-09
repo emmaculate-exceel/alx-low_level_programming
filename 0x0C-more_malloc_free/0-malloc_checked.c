3194cb71c54567ecca7768ee9377e0d368a85c8d
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-08 17:47:13 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+  * malloc_checked - allocates.
+  * @b: size of mem.
+  * Return: pointer to allocated memory.
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;= malloc(b);
+
+	if (pointer == NULL)
+	{
+		exit(98);
+	}
+	return (pointer);
+}
