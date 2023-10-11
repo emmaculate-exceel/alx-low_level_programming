8414ccf3601772874312394a4b8b502ede602366
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-10 17:53:47 -0700

    corretion

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(* d).name = name;
-	(* d).age = age;
-	(* d).owner = owner;
+	d = malloc(sizeof(struct *d));
+
+	if (d == NULL)
+		return (1);
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+	free(d);
 }
