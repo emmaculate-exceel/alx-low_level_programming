a0033b38031900209afedd277a83c9b2dc0b96d1
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-19 20:06:41 -0700

    list:

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-
+	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[%d] (%s)", 0, "nil");
-		}
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
-
+		h = h->next;
 		count++;
 	}
-
 	return (count);
 }
