b36933683f9e7b5eb37e11908cdf290b3a43f08c
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-23 21:46:38 -0700

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index - finds node in a linked list
  * @head : pointer linked list
  * @index: index of the node
- * Return: pointer 
+ * Return: pointer
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
