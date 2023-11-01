625c74f834239813bb2c81860df3e7388fa0e036
emmaculate-exceel <emmaculate2exceel@live.com>
2023-11-01 12:45:48 -0700

    updat

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last value of and input value
+ * @n: input value
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
