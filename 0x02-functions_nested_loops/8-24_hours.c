7863f26e0293094785634e8e1c5eb249d2469f41
emmaculate-exceel <emmaculate2exceel@live.com>
2023-11-25 04:51:19 -0800

    update on 8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,34 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints the minutes of the day
- *
- * Return: Nothing
- */
-
-void jack_bauer()
+* jack_bauer - prints to the output every
+* minutes of the day
+*
+* Return: Always Nothing.
+*/
+void jack_bauer(void)
 {
-	int i, x;
+	int i, x, y, z;
 
+	for (i = 0; i < 3; i++)
+	{
+		for (x = 0; x < 4; x++)
+		{
+			for (y = 0; y < 6; y++)
+			{
+				for (z = 0; z < 10; z++)
+				{
+					if (!(i == 2 && x == 3 && y == 5 && z == 99))
+					{
+						_putchar(i + 48);
+						_putchar(x + 48);
+						_putchar(':');
+						_putchar(y + 48);
+						_putchar(z + 48);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
