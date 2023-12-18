d610d8462240e503afe170fb62a232159adf2e76
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-18 06:26:35 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - the add function
+ *@a: first value
+ *@b: second value
+ *
+ *Return: the total value of both value
+ */
+
+int add(int a, int b)
+{
+  return (a + b);
+}
