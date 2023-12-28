6ae71a4cfebfdc0b9e1d13822d0bc494256e5426
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-27 23:35:51 -0800

    time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,36 @@
 #include "main.h"
-/*
+/**
  *times_table - multiplication table
  *
  *Return: void
  */
 void times_table(void)
 {
-  int rows, cols;
+	int rows, cols;
 
-  for (rows = 0; rows < 10; rows++)
-    {
-      for (cols = 0; cols < 10; cols++)
+	for (rows = 0; rows < 10; rows++)
 	{
-	  int mul = cols * rows;
-	  if (cols != 0)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	  if (mul < 10)
-	    {
-	      _putchar(' ');
-	      _putchar(mul + 48);
-	    }
-	  else
-	    {
-	      _putchar(mul / 10 + 48);
-	      _putchar(mul % 10 + 48);
-	    }
+		for (cols = 0; cols < 10; cols++)
+		{
+			int mul = cols * rows;
+
+			if (cols != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (mul < 10)
+			{
+				_putchar(' ');
+				_putchar(mul + 48)
+			}
+			else
+			}
+				_putchar(mul / 10 + 48);
+				_putchar(mul % 10 + 48)
+			}
+		}
+	_putchar('$');
+	_putchar('\n');
 	}
-      _putchar('$');
-      _putchar('\n');
-    }
 }
