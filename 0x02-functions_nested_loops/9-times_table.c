c315903bea4a3e969557f7dab20c9d8d8022e223
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-27 23:23:48 -0800

    time_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/*
+ *times_table - multiplication table
+ *
+ *Return: void
+ */
+void times_table(void)
+{
+  int rows, cols;
+
+  for (rows = 0; rows < 10; rows++)
+    {
+      for (cols = 0; cols < 10; cols++)
+	{
+	  int mul = cols * rows;
+	  if (cols != 0)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	    }
+	  if (mul < 10)
+	    {
+	      _putchar(mul + 48);
+	    }
+	  else
+	    {
+	      _putchar(mul / 10 + 48);
+	      _putchar(mul % 10 + 48);
+	    }
+	}
+      _putchar('$');
+      _putchar('\n');
+    }
+}
