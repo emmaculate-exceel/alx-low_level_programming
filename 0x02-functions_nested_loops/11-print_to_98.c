de1376e2907df5bda69a670939120e04939f198d
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-01 23:13:06 +0100

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,6 +16,7 @@ void print_to_98(int n)
 			if (i == 98)
 			{
 				printf("%d", i);
+				printf("\n");
 			}
 			else
 			{
@@ -30,6 +31,7 @@ void print_to_98(int n)
 			if (i == 98)
 			{
 				printf("%d", i);
+				printf("\n");
 			}
 			else
 			{
