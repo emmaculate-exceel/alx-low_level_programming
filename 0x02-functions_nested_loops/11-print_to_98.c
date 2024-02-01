4ac1c248d2a64c51510c384cb75eb5dd2c1415ce
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-01 23:08:23 +0100

    to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,41 +1,40 @@
 #include "main.h"
 /**
  *print_to_98 - a function that prints to 98
- *n: the integer that iterates 
+ *@n:the integer that iterates
  *
  *Return: Nothing.
  */
 void print_to_98(int n)
 {
-    int i;
-    
-    if (n <= 98)
-    {
-        for(i = n; i <= 98; i++)
-        {
-            if (i == 98)
-            {
-                printf("%d", i);
-            }
-            else
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-    else
-    {
-            for (i = n; i >= 98; i--)
-            {
-                if (i == 98)
-                {
-                    printf("%d", i);
-                }
-                else
-                {
-                    printf("%d, ", i);
-                }
-            }
-    }
-    
+	int i;
+
+	if (n <= 98)
+	{
+		for(i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
 }
