42837969a0e4364a5fcf2da25477a079973cf695
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-06 13:38:20 +0100

    nth time table; git push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *Print_times_table - print time table nth time
+ *print_times_table - print time table nth time
  *@n: number of times for the rows and the column
  *
  *Return: Void
@@ -8,20 +8,44 @@
 
 void print_times_table(int n)
 {
-  int mul, row, col;
+  void print_times_table(int n)
+{
+	int mul, row, col;
 
-  for (col = 1; col < n; col++)
-    {
-      for (row = 1; row < n; row++)
+	if (n > 15 || n < 0)
 	{
-	  mul = row * col;
-
-	  if (mul < 10 && row != 0)
-	    {
-	      _putchar(mul + 48);
-	      _putchar(44);
-	      _putchar(32);
-	    }
+		printf("Not a valid input");
+		return;
 	}
-    }
+	for (col = 0; col < n; col++)
+	{
+		for (row = 0; row < n ; row++)
+		{
+			mul = row * col;
+
+			if (mul < 10 && row != 0)
+				_putchar(0x20);
+			if (mul != 0)
+			{
+				_putchar(0x20);
+				_putchar(0x2C);
+			}
+			if (mul < 10)
+			{
+				_putchar(mul + 48);
+			}
+			else if (mul < 100)
+			{
+				_putchar(mul / 10 + 48);
+				_putchar(mul % 10 + 48);
+			}
+			else
+			{
+				_putchar((mul / 10) / 10 + 48);
+				_putchar((mul / 10) % 10 + 48);
+				_putchar((mul % 10) % 10 + 48);
+			}
+		}
+		_putchar(0x0A);
+	}
 }
