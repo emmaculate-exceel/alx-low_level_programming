59990aaeb70b96a9f951fa944cbd09536eabfdba
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-16 15:34:27 +0100

    natural numb

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * main - print a total sum of 3 and 5
+  * all the way down to 1024
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			sum += i;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum += i;
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			sum += i;
+		}
+		sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+}
