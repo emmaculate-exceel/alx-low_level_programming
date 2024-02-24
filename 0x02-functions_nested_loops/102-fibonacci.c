88333a74fc0e33a31db0ff0b1c3fa3ef4b7d7617
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-24 07:29:44 +0100

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * fibonacci - executes the fibonacci function
+ *        write a code that runs fibonacci numbers
+ * Return: Always 0;
+ */
+
+void fibonacci(void)
+{
+	int a, b, c = 1;
+
+	for (a = 0; a < 50; a++)
+	{
+		printf("%d, \n", c);
+
+		a = b;
+		b = c;
+		c = a + b;
+	}
+}
+
+/**
+ * main - executes the fibonacci function
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	fibonacci();
+
+	return (0);
+}
