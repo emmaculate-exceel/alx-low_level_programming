3dcc32d65ca44a6e519163734cdf0b5f1400d5b6
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-29 05:22:22 +0100

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - do the sum of fibonacci terms
+ *
+ * Return : Always 0;
+ */
+
+int main(void)
+{
+  long int i, a = 0, b = 1, c, sum;
+
+  for (i = 1; i < 100; i++)
+    {
+      if (i % 2 == 0)
+	{
+	  sum = sum + 1;
+	}
+      a = b;
+      b = c;
+      c = a + b;
+    }
+  printf("%ld\n", sum);
+  return (0);
+}
