2ba45eaf0133c41354b7e3db1039bf017e371f39
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-29 05:46:03 +0100

    1032

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,20 +5,33 @@
  * Return : Always 0;
  */
 
+int fibonacci(int n)
+{
+	long int i, a = 0, b = 1, c, sum;
+
+	for (i = 1; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			sum = sum + 1;
+		}
+	a = b;
+	b = c;
+	c = a + b;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
+
+/**
+ *main - main function for the code
+ *
+ *Return : always 0;
+ */
+
 int main(void)
 {
-  long int i, a = 0, b = 1, c, sum;
+	fibonacci(4000000);
 
-  for (i = 1; i < 34; i++)
-    {
-      if (i % 2 == 0)
-	{
-	  sum = sum + 1;
-	}
-      a = b;
-      b = c;
-      c = a + b;
-    }
-  printf("%ld\n", sum);
-  return (0);
+	return (0);
 }
