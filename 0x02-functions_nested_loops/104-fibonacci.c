965ada1fd9aa572d25f89a902a9ec65b094b3496
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-29 16:01:58 +0100

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints the first 98 fibonacci numbers
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+	unsigned long int i, a, b = 1, c = 1, sum;
+
+	for (i = 1; i < 99; i++)
+	{
+	    printf("%lu, ", c);
+	    a = b;
+	    b = c;
+	    c = a + b;
+	}
+	return (0);
+}
