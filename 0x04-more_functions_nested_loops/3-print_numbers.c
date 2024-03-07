edee6fd5e446c2c80d9556f90adb77b2dbf84153
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-07 23:38:33 +0100

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *main - check the code
+ *print_numbers - print from 0 to 9
  *
  *Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + 48);
