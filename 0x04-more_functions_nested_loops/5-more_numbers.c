ed9211e295f2337687e5def8cbd648377f550602
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-08 14:19:12 +0100

    5-more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print 1 to 14
+ *
+ * Return: always 0;
+ */
+ void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10 + 0x30);
+
+			_putchar(i % 10 + 0x30);
+		}
+
+		_putchar(0x0A);
+	}
+}
