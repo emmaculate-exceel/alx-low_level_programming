d5289de41dd63d1f3c7d61032bd5ada91dcbff76
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-09 05:32:23 +0100

    fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-       			printf("FizzBuzz ");
+       			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%d", i);
+
+		printf(" ");
 	}
 
 	printf("\n");
