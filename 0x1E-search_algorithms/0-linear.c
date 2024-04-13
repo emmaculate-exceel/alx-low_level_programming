e6ab4b7b56c0c024bff2e22001cca234bdc52f55
emmaculate-exceel <emmaculate2exceel@live.com>
2024-04-13 13:26:30 +0100

    linear && Readme

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include 'search_algos.h'
+/**
+ * linear_search - function for searching linearly
+ * @array: the arrays of element to search
+ * @size: the size of element to search
+ * @value: the value being searched for
+ * Return: Always 0
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+
+
+
+}
