5666e41e67fb49d43b5ee4cba078680907307033
emmaculate-exceel <emmaculate2exceel@live.com>
2024-04-15 02:21:26 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,12 +4,24 @@
  * @array: the arrays of element to search
  * @size: the size of element to search
  * @value: the value being searched for
- * Return: Always 0
+ * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t sum;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-
-
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
 }
